6db4d918753d44e8648b3b46f0a2be80e31cd52d
devsingh1504 <k.devsingh14@gmail.com>
2024-09-08 17:15:48 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,7 +11,7 @@ import { persistStore } from "redux-persist";
 let persistor = persistStore(store);
 
 //backend url
-export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = "https://chat-talk-backend.onrender.com";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
